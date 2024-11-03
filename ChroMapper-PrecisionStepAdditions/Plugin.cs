9afd9c952a69bc6b9255af65c41240814fa18741
rynan4818 <14249877+rynan4818@users.noreply.github.com>
2024-11-03 13:17:43 +0900

    プロジェクトファイルを追加します

---
+++ ChroMapper-PrecisionStepAdditions/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace ChroMapper_PrecisionStepAdditions
+{
+    [Plugin("ChroMapper-PrecisionStepAdditions")]
+    public class Plugin
+    {
+        public static Harmony _harmony;
+        public const string HARMONY_ID = "com.github.username.ChroMapper-PrecisionStepAdditions"; //username を各自変更してください
+        [Init]
+        private void Init()
+        {
+            _harmony = new Harmony(HARMONY_ID);
+            _harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Debug.Log("ChroMapper-PrecisionStepAdditions Plugin has loaded!");
+        }
+        [Exit]
+        private void Exit()
+        {
+            _harmony.UnpatchSelf();
+            Debug.Log("ChroMapper-PrecisionStepAdditions Plugin has closed!");
+        }
+    }
+}
